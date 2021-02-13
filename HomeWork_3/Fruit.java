cdee15e81996b3dd4fcf8ace82ce7aa91c5e8cd8
doppel78 <68333251+doppel78@users.noreply.github.com>
2021-02-13 22:38:21 +0300

    HomeWork_3

---
+++ HomeWork_3/Fruit.java
@@ -0,0 +1,13 @@
+package GeekBrains_Java_QA.HomeWork_3;
+
+class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+}
---
+++ HomeWork_3/Orange.java
@@ -0,0 +1,7 @@
+package GeekBrains_Java_QA.HomeWork_3;
+
+class Orange extends Fruit {
+    Orange() {
+        super(1.5f);
+    }
+}
