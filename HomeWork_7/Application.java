d0d638312b3fea3f407f5a4fdfc8d3e506b4c199
doppel78 <68333251+doppel78@users.noreply.github.com>
2021-02-13 22:40:00 +0300

    HomeWork_7

---
+++ HomeWork_7/Application.java
@@ -0,0 +1,10 @@
+package GeekBrains_Java_QA.HomeWork_7;
+
+public class Application {
+    public static void main(String[] args) {
+
+        IUserInterface ui = new UserInterface();
+
+        ui.showMenu();
+    }
+}
---
+++ HomeWork_7/ICityCodeProvider.java
@@ -0,0 +1,7 @@
+package GeekBrains_Java_QA.HomeWork_7;
+
+import java.io.IOException;
+
+public interface ICityCodeProvider {
+    void getCodeByCityName(String cityName) throws IOException;
+}
